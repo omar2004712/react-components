70c4790ec4ea4ec6754f933f4d3813624cb971a0
omar2004712 <o.bahutar@gmail.com>
2022-08-04 09:44:14 +0400

    fix: changes the extensions from .jsx -> .js

--- src/index.js
+++ src/index.js
@@ -3,22 +3,27 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import CommentDetail from './CommentDetail';
 import ApprovalCard from './ApprovalCard';
+import './styles/App.css';
 
 function App() {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        author="Sam"
-        timeAgo="Today at 4:45PM"
-        text="This is a comment"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Tam"
-        timeAgo="Today at 2:00AM"
-        text="This is another comment"
-        avatar={faker.image.avatar()}
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 4:45PM"
+          text="This is a comment"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Tam"
+          timeAgo="Today at 2:00AM"
+          text="This is another comment"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
       <ApprovalCard>
         <CommentDetail
           author="Pam"
